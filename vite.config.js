62b9e70d56871c10a52e29cd559d117aa3df92f9
Thomas Omweri <thomas.omweri@gmail.com>
2025-10-29 07:45:32 +0300

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import tailwind from '@tailwindcss/vite'
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      buildDirectory: 'build',
-      refresh: true,
-    }),
-    tailwind(),
-  ],
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            buildDirectory: 'build',
+            refresh: true,
+        }),
+        tailwind(),
+    ],
+    server: {
+        https: {
+            key: fs.readFileSync(path.resolve(process.env.HOME, '.config/valet/Certificates/laravel-prism.test.key')),
+            cert: fs.readFileSync(path.resolve(process.env.HOME, '.config/valet/Certificates/laravel-prism.test.crt')),
+        },
+        host: 'laravel-prism.test',
+        port: 5173,
+    },
 })
